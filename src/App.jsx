a8575dfecd3db7bf36aa011afd12fc01213f1418
shuvo2o <eng.shuvo03@gmail.com>
2025-12-23 10:00:13 +0600

    ract tailwind template for further use

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-800'>
+      <h1 className=''>Hello This is React Tailwind Template</h1>
+    </div>
+  )
+}
+
+export default App
